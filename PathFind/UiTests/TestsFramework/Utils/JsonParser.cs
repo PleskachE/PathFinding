168750b3c7592d2810b4a77a2c77b5ac0698922b
egorgolock@mail.ru <egorgolock@mail.ru>
2022-02-05 17:39:41 +0300

    Added JsonParser.

---
+++ PathFind/UiTests/TestsFramework/Utils/JsonParser.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace TestsFramework.Utils
+{
+    internal class JsonParser<T>
+    {
+        internal static T Parse(string text) => JsonConvert.DeserializeObject<T>(text);
+    }
+}
